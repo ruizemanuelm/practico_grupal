9eda50a712c1192dbc0c719b5454e73a984543fa
RLauraS <R_LauraS@yahoo.com.ar>
2023-04-26 19:59:36 -0300

    Se guardan los comentarios en local storage

--- js/comentarios.js
+++ js/comentarios.js
@@ -1,4 +1,4 @@
-const contenedorLista = document.getElementById('contenedorLista');
+/*const contenedorLista = document.getElementById('contenedorLista');
 const formInputItem = document.getElementById('formInputItem');
 let item = document.getElementById('item');
 let listaDeItems = [];
@@ -30,3 +30,55 @@ function eliminar (index){
         mostrarLista();
     }
 }
+*/
+const contenedorLista = document.getElementById('contenedorLista');
+const formInputItem = document.getElementById('formInputItem');
+let item = document.getElementById('item');
+let listaDeItems = [];
+formInputItem.addEventListener('submit', agregarItem);
+
+// Cargar la lista de elementos al cargar la página
+window.onload = function() {
+    cargarLista();
+}
+
+function agregarItem(e){
+    e.preventDefault();
+    listaDeItems.push(item.value);
+    formInputItem.reset();
+    mostrarLista();
+    
+    // Guardar la lista de elementos en localStorage
+    localStorage.setItem('listaDeItems', JSON.stringify(listaDeItems));
+}
+
+function mostrarLista(){
+    contenedorLista.innerHTML = '';
+    const lista = document.createElement('ul');
+    contenedorLista.appendChild(lista);
+    listaDeItems.forEach(function (item,index) {
+        const li = document.createElement('li');
+        li.textContent = item;
+        lista.appendChild(li).innerHTML;
+    });
+}
+
+function eliminar (index){
+    let bandera = confirm(`Seguro desea eliminar el item: ${listaDeItems[index]}?`);
+    if(bandera === true){
+        listaDeItems.splice(index,1);
+        mostrarLista();
+        
+        // Actualizar la lista de elementos en localStorage después de eliminar un ítem
+        localStorage.setItem('listaDeItems', JSON.stringify(listaDeItems));
+    }
+}
+
+function cargarLista() {
+    // Cargar la lista de elementos de localStorage
+    if (localStorage.getItem('listaDeItems')) {
+        listaDeItems = JSON.parse(localStorage.getItem('listaDeItems'));
+        mostrarLista();
+    }
+}
+ 
