974059fff4ef516a93d69e1b1b359a2eb8767f37
RLauraS <R_LauraS@yahoo.com.ar>
2023-04-26 17:10:27 -0300

    Se inicia con la vista de los comentarios y la carga de los mismos

---
+++ js/comentarios.js
@@ -0,0 +1,32 @@
+const contenedorLista = document.getElementById('contenedorLista');
+const formInputItem = document.getElementById('formInputItem');
+let item = document.getElementById('item');
+let listaDeItems = [];
+formInputItem.addEventListener('submit', agregarItem);
+
+
+function agregarItem(e){
+    e.preventDefault();
+    listaDeItems.push(item.value);
+    formInputItem.reset();
+    mostrarLista();
+}
+
+function mostrarLista(){
+    contenedorLista.innerHTML = '';
+    const lista = document.createElement('ul');
+    contenedorLista.appendChild(lista);
+    listaDeItems.forEach(function (item,index) {
+        const li = document.createElement('li');
+        li.textContent = item;
+        lista.appendChild(li).innerHTML;
+    });
+}
+
+function eliminar (index){
+    let bandera = confirm(`Seguro desea eliminar el item: ${listaDeItems[index]}?`);
+    if(bandera === true){
+        listaDeItems.splice(index,1);
+        mostrarLista();
+    }
+}
